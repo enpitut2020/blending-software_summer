9f8e2c7cbc438ff482075f1da88b837588b8b767
Masaya Yamada <yamada@kde.cs.tsukuba.ac.jp>
2020-07-24 16:10:11 +0900

    JSONを受け取ってテーブルとして表示する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,5 +8,19 @@ class PostsController < ApplicationController
 
   def result
     @input = params[:search]
+    @return = JSON.parse('{
+      "001": {
+        "channel_name": "HikakinTV",
+        "home_url": "www@wwww",
+        "thumbnail_url": "aaa@@@",
+        "rank_order": 1
+      },
+      "002": {
+        "channel_name": "Hajime",
+        "home_url": "hhh@hhhh",
+        "thumbnail_url": "bbb@@@@",
+        "rank_order": 2
+      }
+    }')
   end
 end
