5207c15c053aa11cb56076efac5d71d163dbb370
Masaya Yamada <yamada@kde.cs.tsukuba.ac.jp>
2020-07-24 16:21:09 +0900

    テスト用JSONデータの更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,17 +9,35 @@ class PostsController < ApplicationController
   def result
     @input = params[:search]
     @return = JSON.parse('{
-      "001": {
-        "channel_name": "HikakinTV",
-        "home_url": "www@wwww",
+      "97361d18": {
+        "channel_name": "兄者弟者",
+        "home_url": "https://www.youtube.com/user/norunine/videos",
         "thumbnail_url": "aaa@@@",
         "rank_order": 1
       },
-      "002": {
-        "channel_name": "Hajime",
-        "home_url": "hhh@hhhh",
+      "29197914": {
+        "channel_name": "おついちTube",
+        "home_url": "https://www.youtube.com/channel/UC6KnFuHBByTI20VENcsoNjg",
         "thumbnail_url": "bbb@@@@",
         "rank_order": 2
+      },
+      "a8acebac": {
+        "channel_name": "SANNINSHOW",
+        "home_url": "https://www.youtube.com/user/sanninshow",
+        "thumbnail_url": "aaa@@@",
+        "rank_order": 1
+      },
+      "2e2619a3": {
+        "channel_name": "わがねこチャンネル",
+        "home_url": "https://www.youtube.com/channel/UC2GuiBNUazRVJv4xmnlcaPA",
+        "thumbnail_url": "aaa@@@",
+        "rank_order": 1
+      },
+      "ba280b05": {
+        "channel_name": "草の人",
+        "home_url": "https://www.youtube.com/channel/UC9WMsQHOevJPa7tAIRAri7g",
+        "thumbnail_url": "aaa@@@",
+        "rank_order": 1
       }
     }')
   end
