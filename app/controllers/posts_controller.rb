ae3532b372417924a1556436024b7bdc853a75da
Masaya Yamada <yamada@kde.cs.tsukuba.ac.jp>
2020-07-28 10:32:02 +0900

    グラフ作成用のデータ整形コードを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,13 @@ class PostsController < ApplicationController
     ret = {"edge":[[{"channel_id":"AAA","channel_name":"case1","home_url":"https://www.youtube.com/1" },{"channel_id":"BBB","channel_name":"case2","home_url":"https://www.youtube.com/2" }],[{"channel_id":"AAA","channel_name":"case1","home_url":"https://www.youtube.com/1" },{"channel_id":"CCC","channel_name":"case3","home_url":"https://www.youtube.com/3" }],[{"channel_id":"CCC","channel_name":"case3","home_url":"https://www.youtube.com/3 "},{"channel_id":"BBB","channel_name":"case2","home_url":"https://www.youtube.com/2" }],[{"channel_id":"CCC","channel_name":"case3","home_url":"https://www.youtube.com/3 "},{"channel_id":"DDD","channel_name":"case4","home_url":"https://www.youtube.com/4 "}]]}
 
     node_seed = {}
+    node_full_seed = {}
     edge = []
     ret[:edge].each do |n|
       node_seed[n[0][:channel_id]] = n[0][:channel_name]
       node_seed[n[1][:channel_id]] = n[1][:channel_name]
+      node_full_seed[n[0][:channel_id]] = n[0]
+      node_full_seed[n[1][:channel_id]] = n[1]
       edge.push({:from=>n[0][:channel_name], :to=>n[1][:channel_name]})
     end
     node = []
@@ -16,6 +19,7 @@ class PostsController < ApplicationController
 
     @edge = edge
     @node = node
+    @node_full = node_full_seed
   end
 
   def search
